b59f951b35b4d44b2665927d812ec00098b13451
jessefrance <jesseluis09@gmail.com>
2026-06-19 00:45:53 +0100

    concluindo o desafio de projetos, aula do curso de lógica.

--- desafio_de_projetos/aulas/index.js
+++ desafio_de_projetos/aulas/index.js
@@ -1,1 +1,36 @@
-console.log("Aula 1 - Introdução ao JavaScript");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Digite o nome do herói: ", (nome) => {
+    rl.question("Digite a quantidade de XP: ", (xp) => {
+
+        xp = Number(xp);
+        let nivel = "";
+
+        if (xp < 1000) {
+            nivel = "Ferro";
+        } else if (xp <= 2000) {
+            nivel = "Bronze";
+        } else if (xp <= 5000) {
+            nivel = "Prata";
+        } else if (xp <= 7000) {
+            nivel = "Ouro";
+        } else if (xp <= 8000) {
+            nivel = "Platina";
+        } else if (xp <= 9000) {
+            nivel = "Ascendente";
+        } else if (xp <= 10000) {
+            nivel = "Imortal";
+        } else {
+            nivel = "Radiante";
+        }
+
+        console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+
+        rl.close();
+    });
+});
